1edd782b3ee9c7d3e8d44fadf96ddab03e817385
Alex Giuliani <giuliani@synchrotron-soleil.fr>
2019-05-21 16:49:44 +0200

    plots.jl adding recipes for chromatogram

--- src/plots.jl
+++ src/plots.jl
@@ -4,6 +4,14 @@ function normalisation(ms::msJ.MScontainer)
     return ms.int .* factor
 end
 
+function normalisation(cr::msJ.Chromatogram)
+    factor = 100. / cr.maxic
+    return cr.ic .* factor
+end
+function scaling(cr::msJ.Chromatogram)
+    return cr.rt ./ 60.
+end
+
 @recipe function f(ms::msJ.MSscan; method = :relative) 
     seriestype --> :path
     seriescolor --> :red
@@ -31,4 +39,21 @@ end
         ylabel --> "Intensity (a.u.)"
     end
     ms.mz, y
+end
+
+@recipe function h(cr::msJ.Chromatogram; method = :relative) 
+    seriestype  --> :path
+    seriescolor --> :blue
+    fillrange   --> 0 
+    fillalpha   --> 0.3
+    label       --> ""
+    xlabel      --> "time (mins)"
+    if method == :relative
+        y = normalisation(cr)
+        ylabel  --> "Intensity (%)"
+    elseif method == :absolute
+        y = cr.ic
+        ylabel  --> "Intensity (a.u.)"
+    end
+    scaling(cr), y
 end
