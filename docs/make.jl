28d01d83e0a7023a79638f15179acd3cddf8400d
Alex Giuliani <giuliani@synchrotron-soleil.fr>
2019-06-02 00:08:55 +0200

    update devbranch to branch release

--- docs/make.jl
+++ docs/make.jl
@@ -30,7 +30,7 @@ makedocs(
 deploydocs(
     repo = "github.com/ajgiuliani/msJ.jl.git",
     target = "build",
-    devbranch = "master",
+    devbranch = "release",
     devurl = "dev",
     versions = ["stable" => "v^", "v#.#", devurl => devurl]
 )
