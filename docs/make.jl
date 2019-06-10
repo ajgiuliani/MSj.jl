edcfe00dc481e87d8b29a18bf1d3a88cf5a26abc
Alex Giuliani <giuliani@synchrotron-soleil.fr>
2019-06-10 13:09:13 +0200

    update doc/make.jl for dev docs

--- docs/make.jl
+++ docs/make.jl
@@ -30,7 +30,7 @@ makedocs(
 deploydocs(
     repo = "github.com/ajgiuliani/msJ.jl.git",
     target = "build",
-#    devbranch = "release",
-#    devurl = "dev",
+    devbranch = "dev",
+    devurl = "dev",
 #    versions = ["stable" => "v^", "v#.#", devurl => devurl]
 )
