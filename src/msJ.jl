c9b0f50c2e6214bac0924902975ce6cd6eaa1616
Alex Giuliani <giuliani@synchrotron-soleil.fr>
2019-10-11 21:01:09 +0200

    getting all dependencies at msJ.jl

--- src/msJ.jl
+++ src/msJ.jl
@@ -5,6 +5,19 @@ Main module for `msJ.jl`-- A Julia package to load and process mass spectrometry
 module msJ
 
 
+using Statistics           # used for Perasons correlation calculation
+using LsqFit               # used for curve fitting
+using DSP                  # used for convolution
+using Interpolations       # used for interpolation
+using LinearAlgebra        # used for matrix operation 
+using LightXML, Codecs     # used for mzXML file import
+using Unicode              # used for file io
+using Plots, RecipesBase   # used for plotting
+
+
+import Base: +, -, *, /
+
+
 include("types.jl")
 include("Io.jl")
 include("msscans.jl")
