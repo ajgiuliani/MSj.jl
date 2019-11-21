dd46e4273a1b4a3eb5ef0ac5c8d1c61f4895fe20
Alex Giuliani <giuliani@synchrotron-soleil.fr>
2019-11-21 21:53:55 +0100

    msJ.jl removing DSP dependency. Other deps for isotopes

--- src/msJ.jl
+++ src/msJ.jl
@@ -7,12 +7,14 @@ module msJ
 
 using Statistics           # used for Perasons correlation calculation
 using LsqFit               # used for curve fitting
-using DSP                  # used for convolution
+using FFTViews             # used for fft
 using Interpolations       # used for interpolation
 using LinearAlgebra        # used for matrix operation 
 using LightXML, Codecs     # used for mzXML file import
 using Unicode              # used for file io
-
+using Combinatorics        # used for factorial & permutations
+using DataStructures       # used for PriorityQueue
+using Printf               # used for @sprintf
 
 import Base: +, -, *, /
 
@@ -24,6 +26,7 @@ include("mzxml.jl")
 include("process.jl")
 include("extract.jl")
 include("utilities.jl")
+include("isotopes.jl")
 
 
 # Submodules
