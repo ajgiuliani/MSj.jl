d253c3ac5e562e319007e833feeb2d8062d129ce
Alex Giuliani <giuliani@synchrotron-soleil.fr>
2019-11-25 09:00:30 +0100

    MSJ.jl

---
+++ src/MSJ.jl
@@ -0,0 +1,39 @@
+"""
+Main module for `msJ.jl`-- A Julia package to load and process mass spectrometry data.
+
+"""
+module MSJ
+
+
+using Statistics           # used for Perasons correlation calculation
+using LsqFit               # used for curve fitting
+using FFTViews             # used for fft
+using Interpolations       # used for interpolation
+using LinearAlgebra        # used for matrix operation 
+using LightXML, Codecs     # used for mzXML file import
+using Unicode              # used for file io
+using Combinatorics        # used for factorial & permutations
+using DataStructures       # used for PriorityQueue
+using Printf               # used for @sprintf
+
+import Base: +, -, *, /
+
+
+include("types.jl")
+include("Io.jl")
+include("msscans.jl")
+include("mzxml.jl")
+include("process.jl")
+include("extract.jl")
+include("utilities.jl")
+include("isotopes.jl")
+
+
+# Submodules
+# ----------
+
+include("plots.jl")
+
+
+
+end # module
